84675c0c5884e1224cfcb842a00ff331c8e9f33a
mehul-anand <89949898+mehul-anand@users.noreply.github.com>
2021-09-26 06:53:44 +0530

    feat : check button

---
+++ app.js
@@ -0,0 +1,6 @@
+const birthDate = document.querySelector("#date-of-birth");
+const userNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check");
+const messageP = document.querySelector("#message");
+
+checkButton.addEventListener("click", checkLucky)
